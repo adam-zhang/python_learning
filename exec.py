d86428c7456d918440366b7e07ceb6bc04f0445e
mingpingzhang <mingpingzhang@163.com>
2015-08-19 10:38:47 +0800

    added execution

---
+++ exec.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#=============================================================================
+#     FileName: exec.py
+#         Desc: 
+#       Author: Mingping (Adam) Zhang
+#        Email: mingpingzhang@163.com
+#     HomePage: 
+#      Version: 0.0.1
+#   LastChange: 2015-08-19 10:27:54
+#      History:
+#=============================================================================
+
+import sys
+import os
+
+def main(argv):
+    os.system("ls --color")
+    
+
+if __name__ == "__main__":
+    main(sys.argv)
